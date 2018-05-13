ff5148c3e462f10afb95efcfca7a13d0d322af90
Steven <xpogosteven@gmail.com>
2018-05-13 00:07:29 -0400

    background changes

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var images = ['url(assets/images/image01.jpg)','url(assets/images/image02.jpg)','url(assets/images/image03.jpg)','url(assets/images/image04.jpg)'];
+var images = ['url(assets/images/image01.jpg)','url(assets/images/image02.jpg)','url(assets/images/image03.jpg)','url(assets/images/image04.jpg)','url(assets/images/image05.jpg)'];
 var current = 0;
 function newBackground(){
   $('body').css('background-image',images[++current % images.length]);
