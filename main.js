6aca6967b20d0a1770385c98523d4b91768aeede
Steven <xpogosteven@gmail.com>
2018-05-13 23:07:12 -0400

    more photos

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-var images = ['url(assets/images/image01.jpg)','url(assets/images/image02.jpg)','url(assets/images/image03.jpg)','url(assets/images/image04.jpg)','url(assets/images/image05.jpg)'];
-var current = 0;
+var images = ['url(assets/images/image01.jpg)','url(assets/images/image02.jpg)','url(assets/images/image03.jpg)','url(assets/images/image04.jpg)',
+'url(assets/images/image05.jpg)','url(assets/images/image06.jpg)','url(assets/images/image07.jpg)','url(assets/images/image08.jpg)','url(assets/images/image09.jpg)',
+'url(assets/images/image10.jpg)'];
+var current = 5;
 function newBackground(){
   $('body').css('background-image',images[++current % images.length]);
   setTimeout(newBackground, 10000);
 }
 $(document).ready(function(){
-  $('body').css('background-image',images[0]);
+  $('body').css('background-image',images[5]);
   $('body').css('background-repeat', 'no-repeat');
   $('body').css('background-size', '120em 60em');
   $('body').css('background-position', 'center');
