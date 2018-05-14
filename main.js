0a07f9c0ee8f012bba9cc085e68e74e241c66135
Steven <xpogosteven@gmail.com>
2018-05-13 23:34:42 -0400

    background changes

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var images = ['url(assets/images/image01.jpg)','url(assets/images/image02.jpg)','url(assets/images/image03.jpg)','url(assets/images/image04.jpg)',
-'url(assets/images/image05.jpg)','url(assets/images/image06.jpg)','url(assets/images/image07.jpg)','url(assets/images/image08.jpg)','url(assets/images/image09.jpg)',
+var images = ['url(assets/images/image02.jpg)','url(assets/images/image03.jpg)','url(assets/images/image04.jpg)',
+'url(assets/images/image05.jpg)','url(assets/images/image06.jpg)','url(assets/images/image07.jpg)','url(assets/images/image09.jpg)',
 'url(assets/images/image10.jpg)'];
 var current = 5;
 function mobilecheck() {
@@ -15,7 +15,7 @@ $(document).ready(function(){
   if(!mobilecheck()){
     $('body').css('background-image',images[5]);
     $('body').css('background-repeat', 'no-repeat');
-    $('body').css('background-size', '120em 60em');
+    $('body').css('background-size', 'cover');
     $('body').css('background-position', 'center');
     $('body').css('transition', 'background-image 1s ease-in-out')
     setTimeout(newBackground, 10000);
